785e37383f1018d2d28a501d69aa9317c0a12d7a
Andrew Glago <andrewglago1@gmail.com>
2021-12-18 01:55:22 +0000

    setup landing

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../assets/logo.png";
+
+const Header = (): JSX.Element => {
+  return (
+    <header className="border-b fixed top-0 w-full">
+      <div className="max-w-[1336px] p-11 m-auto flex justify-between">
+        <Link passHref href="/">
+          <Image src={Logo} alt="March logo" className="cursor-pointer" />
+        </Link>
+        <nav>
+          <button>Log in to March</button>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
