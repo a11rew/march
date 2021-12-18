36bb97705b3d55babb24457ceb26a33547426cf8
Andrew Glago <andrewglago1@gmail.com>
2021-12-18 10:43:53 +0000

    add back button

--- pages/badges/index.tsx
+++ pages/badges/index.tsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 import { NextPage } from "next";
 import Dropdown from "./Dropdown";
 import BadgesDisplay from "./BadgesDisplay";
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { useRouter } from "next/router";
 
 const Badges: NextPage = (): JSX.Element => {
   const [selected, setSelected] = useState<{ name: string }>();
+  const router = useRouter();
 
   return (
-    <div className="h-screen flex flex-col pt-40">
+    <div className="h-screen flex flex-col pt-40 gap-10">
+      <div>
+        <button
+          className="flex items-center gap-2"
+          onClick={() => router.back()}
+        >
+          <IoIosArrowRoundBack className="text-2xl" />
+          <span className="underline">Back</span>
+        </button>
+      </div>
       <div>
         <h1 className="font-medium text-5xl">Roadmap</h1>
         <p className="font-medium mt-2 mb-10">
